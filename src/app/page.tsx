711e6ee6204b840b1de6532990db323b226729a6
Abass Mutala <abassmutala@gmail.com>
2024-02-08 17:02:04 +0000

    added video

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,80 +3,79 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="relative w-screen aspect-video overflow-hidden bg-white">
+    <main className="relative w-screen aspect-video overflow-hidden bg-white">
       <div className="relative h-full z-10">
-        <Image
-          src="/images/bg.jpeg"
-          alt="image"
-          className="absolute top-0 left-0 z-[-1] h-full w-full object-cover object-start"
-          width="1024"
-          height="768"
-        />
-        {/* <div className="container relative mx-auto h-full flex flex-col items-end justify-end gap-12 py-8 md:py-12 lg:py-20">
-          <div className="flex flex-wrap -mx-4">
-            <div className="w-full px-4">
-              <div className="max-w-xl bg-white py-10 px-8 sm:p-12 md:p-12 rounded-md shadow-sm">
-                <h1 className="mb-3 text-3xl font-bold text-[#ED3B25] md:text-5xl">
-                  Now your are here
-                  <br />
-                  Let&apos;s talk business!
-                </h1>
-                <p className="mb-8 text-base font-medium text-body-color lg:mb-10">
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet
-                  cupiditate expedita quia eos odit laborum neque ut nisi
-                  adipisci totam.
-                </p>
-                <div className="flex flex-wrap gap-3">
-                  <Link
-                    href="/data/MOOVE With Pageline.pdf"
-                    target="_blank"
-                    className="group inline-flex items-center text-base font-medium bg-white shadow-1 text-orange-600 hover:text-primary hover:underline"
-                  >
-                    Download our Info Pack
-                    <span className="ml-2">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        height="20"
-                        viewBox="0 -960 960 960"
-                        width="20"
-                        className="fill-[#ED3B25] group-hover:transition-all duration-300 ease-in-out"
+        <video width="1024" height="768" autoPlay muted loop className="absolute inset-0 w-full h-full -z-10">
+          <source src="/images/bg-video.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        <div className="absolute inset-0 bg-black/30 -z-10"></div>
+        <section className="h-full max-w-[1440px] mx-auto px-8 z-10">
+          <div className="w-full h-full flex justify-end">
+            <div className="w-full h-full flex flex-col max-w-lg">
+              <div className="h-full flex flex-col justify-between">
+                <div className="w-full bg-white pt-16 p-8 rounded-b-2xl shadow-sm">
+                  <div className="w-full flex items-center justify-between">
+                    <div className="h-8">
+                      <Image
+                        src="/images/moove-logo.png"
+                        alt="moove logo"
+                        className="h-8 w-full object-contain object-center"
+                        width="1024"
+                        height="768"
+                      />
+                    </div>
+                    <div className="h-12">
+                      <Image
+                        src="/images/pageline-logo.png"
+                        alt="pageline logo"
+                        className="h-12 w-full object-contain object-center"
+                        width="1024"
+                        height="768"
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className="lg:pb-28">
+                  <div className="w-full bg-[#ED3B25] p-8 rounded-2xl shadow-sm">
+                    <h1 className="mb-3 text-3xl leading-6 font-bold text-white md:text-4xl">
+                      Now your are here
+                      <br />
+                      Let&apos;s talk business!
+                    </h1>
+                    <p className="mb-8 text-base font-medium text-white lg:mb-24">
+                      Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                      Amet cupiditate expedita quia eos odit laborum neque ut
+                      nisi adipisci totam.
+                    </p>
+                    <div className="flex flex-wrap gap-3">
+                      <Link
+                        href="/data/MOOVE With Pageline.pdf"
+                        target="_blank"
+                        className="group inline-flex items-center text-base font-medium text-white hover:underline"
                       >
-                        <path d="M440-800v487L216-537l-56 57 320 320 320-320-56-57-224 224v-487h-80Z" />
-                      </svg>
-                    </span>
-                  </Link>
+                        Download our Info Pack
+                        <span className="ml-2">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            height="20"
+                            viewBox="0 -960 960 960"
+                            width="20"
+                            className="fill-white group-hover:transition-all duration-300 ease-in-out"
+                          >
+                            <path d="M440-800v487L216-537l-56 57 320 320 320-320-56-57-224 224v-487h-80Z" />
+                          </svg>
+                        </span>
+                      </Link>
+                    </div>
+                  </div>
                 </div>
+                {/* <div></div> */}
               </div>
             </div>
           </div>
-          <div className="flex flex-wrap -mx-4">
-            <div className="w-full px-4">
-              <div className="w-[576px] bg-white py-10 px-8 sm:p-12 md:p-12 rounded-md shadow-sm">
-                <div className="w-full flex items-center justify-end gap-6 md:gap-8">
-                  <div className="h-8">
-                    <Image
-                      src="/images/moove-logo.png"
-                      alt="moove logo"
-                      className="h-full w-full object-contain object-center"
-                      width="1024"
-                      height="768"
-                    />
-                  </div>
-                  <div className="h-10">
-                    <Image
-                      src="/images/pageline-logo.png"
-                      alt="pageline logo"
-                      className="h-full w-full object-contain object-center"
-                      width="1024"
-                      height="768"
-                    />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div> */}
+        </section>
       </div>
-    </div>
+    </main>
   );
 }
